def9008e5f0a8360274b60aec394ab13dbee7adf
UwU <bblueblack91@gmail.com>
2023-06-28 23:55:25 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+function submit() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    console.log(username);
+    console.log(password);
+    if(username == "korn" || password == "ewwwww") {
+      location.href = "home.html"
+    }else {
+ 
+    }
+  }
